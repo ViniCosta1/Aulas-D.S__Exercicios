f87a649ff6532df70b42d4a10bdcb504e6169228
ViniCosta1 <vinicosta154@hotmail.com>
2022-03-16 08:41:32 -0300

    Update file (Added elseif)

--- if_ifelse.php
+++ if_ifelse.php
@@ -2,4 +2,16 @@
 $dia = 10;
 $quinz = ($dia >= 15) ? "2ª quinzena" : "1ª quinzena";
 
-echo "$dia pertence a $quinz";
+echo "$dia º dia pertence a $quinz";
+
+////////////////////////////////////////////////////////////////////////////////////////
+echo "<hr>";
+
+$dia = 15;
+if ($dia < 10):
+    echo "$dia º dia pertence a 1ª quinzena";
+elseif ($dia == 15):
+    echo "$dia º dia pertence ao meio do mês";
+else:
+    echo "$dia º dia pertence a 2ª quinzena";
+endif;
