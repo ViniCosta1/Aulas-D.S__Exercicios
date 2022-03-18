95e4a5e87b9aa79493e3f089372e177cb441644f
ViniCosta1 <vinicosta154@hotmail.com>
2022-03-18 08:11:49 -0300

    Method GET in forms (working)

--- php and html/envio_dados.php
+++ php and html/envio_dados.php
@@ -1,5 +1,21 @@
 <?php
+
 $nota1 = $_GET['nota1'];
 $nota2 = $_GET['nota2'];
+$nome = $_GET['nome'];
 
-echo $nota1;
+$med = ($nota1 + $nota2)/2;
+
+if (($med % 2) == 0):
+    echo "$nome, sua média foi $med. (Par)";
+else:
+    echo "$nome, sua média foi $med. (Ímpar)";
+endif;
+ 
+if ($med >= 7):
+    echo "<br>";
+    echo "Você foi aprovado!";
+else:
+    echo "<br>";
+    echo "Você foi reprovado";
+endif;
