be002fbc0aaa20b27444a980a2d8fd57f2aad2e4
ViniCosta1 <vinicosta154@hotmail.com>
2022-03-18 08:30:08 -0300

    Update GET method

--- php and html/envio_dados.php
+++ php and html/envio_dados.php
@@ -1,11 +1,10 @@
 <?php
 
 $nota1 = $_GET['nota1'];
-$nota2 = $_GET['nota2'];
-$nome = $_GET['nome'];
+// $nota2 = $_GET['nota2'];
+// $nome = $_GET['nome'];
 
-$med = ($nota1 + $nota2)/2;
-
+/*
 if (($med % 2) == 0):
     echo "$nome, sua média foi $med. (Par)";
 else:
@@ -18,4 +17,13 @@ if ($med >= 7):
 else:
     echo "<br>";
     echo "Você foi reprovado";
+endif;
+*/
+
+if ($nota1 == 0):
+    echo "Esse valor é igual a 0";
+elseif ($nota1 > 0):
+    echo "Esse valor, $nota1, é positivo";
+else:
+    echo "Esse valor, $nota1, é negativo";
 endif;
