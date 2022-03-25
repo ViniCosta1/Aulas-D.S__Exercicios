3704ebd55ca0505322be3a336a741ee47ee1312c
ViniCosta1 <vinicosta154@hotmail.com>
2022-03-25 07:34:10 -0300

    Added file idade.php

---
+++ idade.php
@@ -0,0 +1,38 @@
+<html>
+<body>
+<?php
+
+if (isset($_GET['enviar-forms'])):
+    $ano = $_GET['ano'];
+    $nome = $_GET['nome'];
+
+    $idade = 2022 - $ano;
+
+
+    if ($idade >= 16 && $idade < 18):
+        echo "$nome, você tem direito de voto FACULTATIVO<br>";
+    elseif ($idade >= 18 && $idade <= 65):
+        echo "$nome, seu voto é obrigatório<br>";
+    elseif ($idade > 65):
+        echo "$nome, seu voto é facultativo<br>";
+    endif;
+
+
+    if ($idade >= 18):
+        echo "$nome, você pode dirigir";
+    else:
+        echo "$nome, você não pode dirigir";
+    endif;
+endif;
+
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
+            Digite seu ano de nascimento <input type="text" name="ano" id=""> <br>
+
+            Digite seu nome <input type="text" name="nome" id=""> <br>
+
+            <input type="submit" value="Somar" name = "enviar-forms">
+        </form>
+</body>
+</html>
