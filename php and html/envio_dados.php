fc0545a9f79757a29a5fdfdecffa38aee2c3db5a
ViniCosta1 <vinicosta154@hotmail.com>
2022-04-01 08:26:12 -0300

    some exercises

--- php and html/envio_dados.php
+++ php and html/envio_dados.php
@@ -1,29 +1,24 @@
 <?php
 
-$nota1 = $_GET['nota1'];
-// $nota2 = $_GET['nota2'];
-// $nome = $_GET['nome'];
+$nome = $_GET['nome'];
+$altura = $_GET['altura'];
+$peso = $_GET['peso'];
 
-/*
-if (($med % 2) == 0):
-    echo "$nome, sua média foi $med. (Par)";
-else:
-    echo "$nome, sua média foi $med. (Ímpar)";
-endif;
- 
-if ($med >= 7):
-    echo "<br>";
-    echo "Você foi aprovado!";
-else:
-    echo "<br>";
-    echo "Você foi reprovado";
-endif;
-*/
+$imc = $peso / ($altura**2);
 
-if ($nota1 == 0):
-    echo "Esse valor é igual a 0";
-elseif ($nota1 > 0):
-    echo "Esse valor, $nota1, é positivo";
-else:
-    echo "Esse valor, $nota1, é negativo";
+echo "<h1>Calculadora de IMC</h1>";
+echo "Seu IMC é $imc <br>";
+
+if ($imc < 18.5 ):
+    echo "Você está abaixo do Peso";
+elseif ($imc >= 18.5 && $imc < 25):
+    echo "Você está com o peso normal";
+elseif ($imc >= 25 && $imc < 30):
+    echo "Você está com sobrepeso";
+elseif ($imc >= 30 && $imc < 35):
+    echo "Você está com Obesidade grau I";
+elseif ($imc >= 35 && $imc < 40):
+    echo "Você está com Obesidade grau II";
+elseif ($imc >= 40):
+    echo "Você está com Obesidade grau III";
 endif;
